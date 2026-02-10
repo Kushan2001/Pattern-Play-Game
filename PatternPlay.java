624f3e623af33266204c27cf75de5aabe81ba336
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-02-10 20:12:09 +0530

    Create PatternPlay window using Java Swing

---
+++ PatternPlay.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class PatternPlay {
+    PatternPlay() {
+        JFrame frame = new JFrame("Pattern Play");
+        frame.setSize(600, 650);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+ 
