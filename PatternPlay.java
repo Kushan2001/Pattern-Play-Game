55445d1fe302d92f6db34a8e4f06a82af3f12507
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-02-10 20:30:21 +0530

    Add game state and player variables

--- PatternPlay.java
+++ PatternPlay.java
@@ -10,5 +10,14 @@ public class PatternPlay {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+    JButton[][] board = new JButton[3][3];
+    String playerX = "X";
+    String playerO = "O";
+    String currentPlayer = playerX;
+
+    boolean gameOver = false;
+    int turns = 0;
+
 }
  
